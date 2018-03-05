3eefc925704c993807b8fe9faa1d0872c103e079
Marco Canducci <marco.candoz@gmail.com>
2018-03-05 19:10:59 +0100

    improved l_pattern/4 and adjacent/4.

--- space_2d.pl
+++ space_2d.pl
@@ -2,6 +2,7 @@
 %%         Projection of the vector (X0,Y0) -> (X,Y) on N/S/W/E axes          %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
 %north_projection(+X0, +Y0, +X, +Y, ?PROJ)
 north_projection(_, Y0, _, Y, PROJ) :- PROJ is Y - Y0.
 
@@ -14,122 +15,96 @@ east_projection(X0, _, X, _, PROJ) :- PROJ is X - X0.
 %west_projection(+X0, +Y0, +X, +Y, ?PROJ)
 west_projection(X0, _, X, _, PROJ) :- PROJ is X0 - X.
 
-%north(+X0, +Y0, +X, +Y)
-north(X0, Y0, X, Y) :- X =:= X0, Y > Y0.
-
-%% Deprecated:
-
-%south(+X0, +Y0, +X, +Y)
-south(X0, Y0, X, Y) :- X =:= X0, Y < Y0.
-
-%west(+X0, +Y0, +X, +Y)
-west(X0, Y0, X, Y) :- X < X0, Y =:= Y0.
-
-%east(+X0, +Y0, +X, +Y)
-east(X0, Y0, X, Y) :- X > X0, Y =:= Y0.
-
-%north_east(+X0, +Y0, +X, +Y)
-north_east(X0, Y0, X, Y) :- X > X0, Y > Y0.
-
-%south_east(+X0, +Y0, +X, +Y)
-south_east(X0, Y0, X, Y) :- X > X0, Y < Y0.
-
-%south_west(+X0, +Y0, +X, +Y)
-south_west(X0, Y0, X, Y) :- X < X0, Y < Y0.
-
-%north_west(+X0, +Y0, +X, +Y)
-north_west(X0, Y0, X, Y) :- X < X0, Y > Y0.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%      Simple 2D geometry: relevant lines between (X1, Y1) and (X2, Y2)      %%
+%%          Move in one particular direction from (X0,Y0) to (X,Y)            %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% horizontal(+X1, +Y1, +X2, +Y2)
-horizontal(X1, Y1, X2, Y2) :- X1 =\= X2, Y1 =:= Y2.
 
-% vertical(+X1, +Y1, +X2, +Y2)
-horizontal(X1, Y1, X2, Y2) :- X1 =\= X2, Y1 =:= Y2.
+% one_step_north(+X0, +Y0, ?X, ?Y)
+one_step_north(X0, Y0, X, Y) :- X = X0, Y is Y0 + 1.
 
-% horizontal(+X1, +Y1, +X2, +Y2)
-horizontal(X1, Y1, X2, Y2) :- X1 =\= X2, Y1 =:= Y2.
+% one_step_south(+X0, +Y0, ?X, ?Y)
+one_step_south(X0, Y0, X, Y) :- X = X0, Y is Y0 - 1.
 
-% horizontal(+X1, +Y1, +X2, +Y2)
-horizontal(X1, Y1, X2, Y2) :- X1 =\= X2, Y1 =:= Y2.
+% one_step_east(+X0, +Y0, ?X, ?Y)
+one_step_east(X0, Y0, X, Y) :- X is X0 + 1, Y = Y0.
+
+% one_step_west(+X0, +Y0, ?X, ?Y)
+one_step_west(X0, Y0, X, Y) :- X is X0 - 1, Y = Y0.
+
+
+% two_steps_north(+X0, +Y0, ?X, ?Y)
+two_steps_north(X0, Y0, X, Y) :- X = X0, Y is Y0 + 2.
+
+% two_steps_south(+X0, +Y0, ?X, ?Y)
+two_steps_south(X0, Y0, X, Y) :- X = X0, Y is Y0 - 2.
+
+% two_steps_east(+X0, +Y0, ?X, ?Y)
+two_steps_east(X0, Y0, X, Y) :- X is X0 + 2, Y = Y0.
+
+% two_steps_west(+X0, +Y0, ?X, ?Y)
+two_steps_west(X0, Y0, X, Y) :- X is X0 - 2, Y = Y0.
+
+
+% n_steps_north(+N, +X0, +Y0, ?X, ?Y)
+n_steps_north(X0, Y0, X, Y) :- X = X0, Y is Y0 + N.
+
+% n_steps_south(+N, +X0, +Y0, ?X, ?Y)
+n_steps_south(X0, Y0, X, Y) :- X = X0, Y is Y0 - N.
+
+% n_steps_east(+N, +X0, +Y0, ?X, ?Y)
+n_steps_east(X0, Y0, X, Y) :- X is X0 + N, Y = Y0.
+
+% n_steps_west(+N, +X0, +Y0, ?X, ?Y)
+n_steps_west(X0, Y0, X, Y) :- X is X0 - N, Y = Y0.
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                             Miscellaneous                                  %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
 %adjacent(+X0, +Y0, ?X, ?Y)
 adjacent(X0, Y0, X, Y) :-
-    (X is X0 - 1; X is X0; X is X0 + 1), 
-    (Y is Y0 - 1; Y is Y0; Y is Y0 + 1),
-    not (X = X0, Y = Y0).  % comment out this subgoal to include the soultion where (X1,Y1) is the same point as (X2,Y2)
+  (X is X0 - 1; X is X0; X is X0 + 1), 
+  (Y is Y0 - 1; Y is Y0; Y is Y0 + 1),
+  not (X = X0, Y = Y0).  % comment out this subgoal to include the soultion where (X,Y) is the same point as (X0,Y0)
 
-%deprecated_adjacent(+X1, +Y1, +X2, +Y2)
-deprecated_adjacent(X1, Y1, X2, Y2) :-
+%% In this previous version the 3rd and 4th parameter should have been mandatory instantiated. I found that to be an unnecessary limit. 
+%adjacent_old(+X1, +Y1, +X2, +Y2)
+adjacent_old(X1, Y1, X2, Y2) :-
   north_projection(_, Y1, _, Y2, N_PROJ), (abs(N_PROJ) =:= 0; abs(N_PROJ) =:= 1),
   east_projection(X1, _, X2, _, E_PROJ), (abs(E_PROJ) =:= 0; abs(E_PROJ) =:= 1),
-  ((X1 =\= X2); (Y1 =\= Y2)).  % comment out this subgoal to include the soultion where (X1,Y1) is the same point as (X2,Y2)
+  ((X1 =\= X2); (Y1 =\= Y2)).  % comment out this subgoal to include the soultion where (X2,Y2) is the same point as (X1,Y1)
+
 
 %l_pattern(+X0, +Y0, ?X, ?Y)
 l_pattern(X0, Y0, X, Y) :-
   (
-    (north_projection(X0, Y0, A, B, 2); north_projection(X0, Y0, A, B, -2)),
-    (east_projection(X0, Y0, A, B, 1); east_projection(X0, Y0, A, B, -1))
+    (X is X0 + 2; X is X0 - 2),
+    (Y is Y0 + 1; Y is Y0 - 1)
   );
   (
-    (north_projection(X0, Y0, A, B, 1); north_projection(X0, Y0, A, B, -1)),
-    (east_projection(X0, Y0, A, B, 2); east_projection(X0, Y0, A, B, -2))
+    (X is X0 + 1; X is X0 - 1),
+    (Y is Y0 + 2; Y is Y0 - 2)
   ).
 
-%distance(+X1, +Y1, +X2, +Y2, -D)
-distance(X1, Y1, X2, Y2, D) :- D is sqrt((X2-X1)^2 + (Y2-Y1)^2).
-
-%%% Relative direction predicates %%% (Ahead, Behind, )
-
-% one_cell_ahead(+BEFORE_X, +BEFORE_Y, ?AFTER_X, ?AFTER_Y)
-one_cell_ahead(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :- 
-  AFTER_X = BEFORE_X,
-  ((turn(white), AFTER_Y is BEFORE_Y+1) ; (turn(black), AFTER_Y is BEFORE_Y-1)).
-
-% one_cell_behind(+BEFORE_X, +BEFORE_Y, ?AFTER_X, ?AFTER_Y)
-one_cell_behind(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :- 
-  AFTER_X = BEFORE_X,
-  ((turn(white), AFTER_Y is BEFORE_Y-1) ; (turn(black), AFTER_Y is BEFORE_Y+1)).
-
-% one_cell_right(+BEFORE_X, +BEFORE_Y, ?AFTER_X, ?AFTER_Y)
-one_cell_right(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :- 
-  ((turn(white), AFTER_X is BEFORE_X+1) ; (turn(black), AFTER_X is BEFORE_X-1)),
-  AFTER_Y = BEFORE_Y.
-
-% one_cell_left(+BEFORE_X, +BEFORE_Y, ?AFTER_X, ?AFTER_Y)
-one_cell_left(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :- 
-  ((turn(white), AFTER_X is BEFORE_X-1) ; (turn(black), AFTER_X is BEFORE_X+1)),
-  AFTER_Y = BEFORE_Y.
-
-% in_line(+BEFORE_X, +BEFORE_Y, ?AFTER_X, ?AFTER_Y)
-in_line(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :-
-  AFTER_X = BEFORE_X ; AFTER_Y = BEFORE_Y.
-
-% in_diagonal(+BEFORE_X, +BEFORE_Y, +AFTER_X, +AFTER_Y)
-in_diagonal(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :-
-  ((AFTER_Y - BEFORE_Y) =:= (AFTER_X - BEFORE_X)); % same diagonal
-  ((AFTER_Y - BEFORE_Y) =:= (BEFORE_X - AFTER_X)). % same anti-diagonal
-
-path_is_clear(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :-  % a path can be a straight or a diagonal line
+%% Equivalent but slightly less efficient version of l_pattern/4.
+%l_pattern_old(+X0, +Y0, ?X, ?Y)
+l_pattern_old(X0, Y0, X, Y) :-
   (
-    in_diagonal(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y),
-    north_east_is_clear
-  ). %% TODO
- 
-diagonal_is_free(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y) :-
-  in_diagonal(BEFORE_X, BEFORE_Y, AFTER_X, AFTER_Y),  % this check can be moved outside
+    (two_steps_north(X0, Y0, A, B); two_steps_south(X0, Y0, A, B)),
+    (one_step_east(A, B, X, Y); one_step_west(A, B, X, Y))
+  );
   (
-    AFTER_X > BEFORE_X,
-    (
-      (AFTER_Y > BEFORE_Y, diagonal_is_free(BEFORE_X, BEFORE_Y, AFTER_X-1, AFTER_Y-1))
-    )
+    (one_step_north(X0, Y0, A, B); one_step_south(X0, Y0, A, B)),
+    (two_steps_east(A, B, X, Y); two_steps_west(A, B, X, Y))
   ).
+
+
+%in_between(+X1, +Y1, +X2, +Y2, -X, -Y)
+in_between(X1, Y1, X2, Y2) :-
+  
